9149f9568e4008649c7b990b3902ebef411485f5
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-04-08 00:51:35 -0400

    Graphql - small fix

--- frontend/frontend/src/app/GraphQLModule.ts
+++ frontend/frontend/src/app/GraphQLModule.ts
@@ -1,25 +1,20 @@
-import {NgModule} from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
-import {ApolloModule, Apollo} from 'apollo-angular';
-// import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
-import { HttpLink } from "apollo-angular/http";
-import {InMemoryCache} from 'apollo-cache-inmemory';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache, ApolloCache } from '@apollo/client/core';
 
 const uri = 'http://localhost:5000/graphql'; // <-- add the URL of the GraphQL server here
 
 @NgModule({
-    exports: [],
-    imports: [
-        HttpClientModule,
-        ApolloModule,
-        // HttpLinkModule
-    ],
+  exports: [],
+  imports: [HttpClientModule, ApolloModule],
 })
-    export class GraphQLModule {
-        constructor({ apollo, httpLink }: { apollo: Apollo; httpLink: HttpLink; }) {
-            apollo.create({
-                link: httpLink.create({uri}),
-                cache: new InMemoryCache()
-            });
-        }
-    }
+export class GraphQLModule {
+  constructor(apollo: Apollo, httpLink: HttpLink) {
+    apollo.create({
+      link: httpLink.create({ uri }),
+      cache: new InMemoryCache() as ApolloCache<unknown>,
+    });
+  }
+}
