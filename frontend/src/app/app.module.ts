4c0b2699a1497513dbd81a7888393d8cf24a00a7
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-04-09 02:19:47 -0400

    Graphql connected

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import * as apolloAngularLinkHttp from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    ApolloModule,
+    apolloAngularLinkHttp.HttpLinkModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  declarations: [
+    LoginComponent,
+    SignupComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(apollo: Apollo, httpLink: apolloAngularLinkHttp.HttpLink) {
+    apollo.create({
+      link: httpLink.create({uri: 'backendopx.herokuapp.com/graphql'}),
+      cache: new InMemoryCache()
+    });
+  }
+}
